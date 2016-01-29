2f65fc2785c517e9aa2ec3093507644e6a2af444
Alex Beals <dado3212@users.noreply.github.com>
2016-01-29 11:39:13 -0500

    Code refactoring
    Added in comments
    Renamed 'url' to 'base_url' for clarity

--- vote.py
+++ vote.py
@@ -1,24 +1,32 @@
 import requests, re, json, time
 requests.packages.urllib3.disable_warnings()
 
-url = 'https://polldaddy.com/poll/'
+# Created by Alex Beals
+# Last updated: January 29th, 2016
+
+base_url = 'https://polldaddy.com/poll/'
 redirect = ""
 
 def vote_once(form, value):
 	c = requests.Session()
-	init = c.get(url + str(form) + "/", headers=redirect, verify=False)
+	init = c.get(base_url + str(form) + "/", headers=redirect, verify=False)
+	# Search for the data-vote JSON object
 	data = re.search("data-vote=\"(.*?)\"",init.text).group(1).replace('&quot;','"')
 	data = json.loads(data)
+	# Search for the hidden form value
 	pz = re.search("type='hidden' name='pz' value='(.*?)'",init.text).group(1)
+	# Build the GET url to vote
 	request = "https://polldaddy.com/vote.php?va=" + str(data['at']) + "&pt=0&r=0&p=" + str(form) + "&a=" + str(value) + "%2C&o=&t=" + str(data['t']) + "&token=" + str(data['n']) + "&pz=" + str(pz)
 	send = c.get(request, headers=redirect, verify=False)
 	return ('revoted' in send.url)
 
 def vote(form, value, times):
 	global redirect
-	redirect = {"Referer": url + str(form) + "/", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36", "Upgrade-Insecure-Requests":"1", "Accept-Encoding": "gzip, deflate, sdch", "Accept-Language": "en-US,en;q=0.8"}
+	redirect = {"Referer": base_url + str(form) + "/", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36", "Upgrade-Insecure-Requests":"1", "Accept-Encoding": "gzip, deflate, sdch", "Accept-Language": "en-US,en;q=0.8"}
+	# For each voting attempt
 	for i in xrange(1,times+1):
 		b = vote_once(form, value)
+		# If successful, print that out, else try waiting for 60 seconds (rate limiting)
 		if not b:
 			print "Voted (time number " + str(i) + ")!"
 			time.sleep(3)
@@ -27,6 +35,7 @@ def vote(form, value, times):
 			i-=1
 			time.sleep(60)
 
+# Initialize these to the specific form and how often you want to vote
 poll_id = 0
 answer_id = 0
 number_of_votes = 0
